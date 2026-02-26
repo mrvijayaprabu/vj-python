17d332cd37390af668955e9846c47b86176f64e2
VJ <76964134+mrvijayaprabu@users.noreply.github.com>
2026-02-26 21:17:29 +0400

    Add files via upload

---
+++ sample.py
@@ -0,0 +1,10 @@
+print("""Hi Team, I have completed the following tasks:
+1. Github account creation
+2. Profile picture changed
+3. Repository creation
+4. Repository profile shared as public
+5. Sample file uploaded
+6. Committed the changes
+7. Shared the profile link: https://github.com/mrvijayaprabu/vj-python
+  
+  Thanks.""")
